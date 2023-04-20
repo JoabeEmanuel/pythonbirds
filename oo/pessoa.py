7b8f4a2881bef8f221b9c5539a91bad2c5f839d6
JoabeEmanuel <131379359+JoabeEmanuel@users.noreply.github.com>
2023-04-20 14:15:56 -0300

    Update pessoa.py

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,2 +1,27 @@
 class Pessoa:
-    pass
+    def __int__(self):
+        self.nome = None
+
+    def cumprimentar(self):
+        return f'Olá {id(self)}'
+
+
+if __name__ == '__main__':
+    p = Pessoa()
+    print(Pessoa.cumprimentar(p))
+    print(id(p))
+    print(p.cumprimentar())
+    print(p.nome)class Pessoa:
+    def __int__(self):
+        self.nome = None
+
+    def cumprimentar(self):
+        return f'Olá {id(self)}'
+
+
+if __name__ == '__main__':
+    p = Pessoa()
+    print(Pessoa.cumprimentar(p))
+    print(id(p))
+    print(p.cumprimentar())
+    print(p.nome)
